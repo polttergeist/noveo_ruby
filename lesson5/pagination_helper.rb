a3b7a6655b19233a8959c677ffa5d222a3155cd3
polttergeist <ivan010201@gmail.com>
2022-07-25 16:32:12 +0300

    Update pagination_helper.rb

--- lesson5/pagination_helper.rb
+++ lesson5/pagination_helper.rb
@@ -1,29 +1,29 @@
 class PaginationHelper
+  attr_accessor :unpaginated
+  attr_reader :item_count, :count
+
   def initialize(arr, count)
-    @paginated = []
+    @unpaginated = arr
     @item_count = arr.length
     @count = count
-    while arr.length > 0 do
-      @paginated.append(arr.shift(count))
-    end
   end
 
   def page_count
-    @paginated.length
-  end
-
-  def item_count
-    @item_count
+    (item_count.to_f / count).ceil
   end
 
   def page_item_count(i)
-    return -1 unless i >= 0 and i < self.page_count
-    @paginated[i].length
+    return -1 unless i >= 0 && i < self.page_count
+    if item_count <= count
+      item_count
+    else
+      unpaginated[i*count...(i+1)*count].length
+    end
   end
 
   def page_index(i)
-    return -1 unless i >= 0 and i < self.item_count
-    i / @count
+    return -1 unless i >= 0 && i < self.item_count
+    i / count
   end
 end
 
