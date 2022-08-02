b47fe0fa894c9bb64124952da1d93fa213fdc8b7
polttergeist <ivan010201@gmail.com>
2022-08-02 23:24:35 +0300

    Create indifferent_accessable.rb

---
+++ lesson8/indifferent_accessable.rb
@@ -0,0 +1,17 @@
+module IndifferentAccessable
+  def [](key)
+    return super(key) unless super(key).nil?
+    if key.class == Symbol
+      super(key.to_s)
+    elsif key.class == String
+      super(key.to_sym)
+    end
+  end
+end
+
+class Hash
+  def make_indifferent_accessable!
+    self.singleton_class.include(IndifferentAccessable)
+    self
+  end
+end
